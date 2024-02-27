c6c237a7b4d5aa7865e5433516047f8e494ef64e
FilipeMPinto91 <152388850+FilipeMPinto91@users.noreply.github.com>
2024-02-27 14:17:43 +0000

    Added duck death animation

--- scripts/duck_green.js
+++ scripts/duck_green.js
@@ -2,20 +2,29 @@ document.addEventListener(`DOMContentLoaded`,() => {
     const canvas = document.getElementById(`canvas1`);
     const ctx = canvas.getContext(`2d`);
     const duckSprites = [];
+    const fallingDuckSprites = [];
     let currentFrameIndex = 0;
     const duckWidth = 80;
     const duckHeight = 50;
     let duckX = Math.random() * (canvas.width - duckWidth);
     let duckY = Math.random() * (canvas.height - duckHeight);
     let isDuckAlive = true;
+    let duckSpeedX = Math.random() * 10 -5;
+    let duckSpeedY = Math.random() * 10 -5;
 
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 
     for (let i =0; i < 3; i++ ) {
         const duckImg = new Image();
-        duckImg.src = `Images/GreenDuckH${i}.png`;
+        duckImg.src = `assets/images/GreenDuckH${i}.png`;
         duckSprites.push(duckImg);
+    }
+
+    for (let i = 0; i < 4; i++) {
+        const fallingDuckImg = new Image();
+        fallingDuckImg.src = `assets/images/GreenDuckF${i}.png`
+        fallingDuckSprites.push(fallingDuckImg);
     }
 
     const drawDuck = () => {
@@ -32,20 +41,57 @@ document.addEventListener(`DOMContentLoaded`,() => {
             mouseY >= duckY && mouseY <= duckY + duckHeight && isDuckAlive) {
                 isDuckAlive = false;
 
+                animateFallingDuck();
+
                 duckX = Math.random() * (canvas.width - duckWidth);
                 duckY = Math.random() * (canvas.height - duckHeight);
                 isDuckAlive = true;
             }
     };
 
+    const updateDuckPosition = () => {
+        duckX += duckSpeedX;
+        duckY += duckSpeedY;
+        if (duckX < 0 || duckX + duckWidth > canvas.width) {
+            duckSpeedX *= -1;
+        }
+        if (duckY < 0 || duckY + duckHeight > canvas.height) {
+            duckSpeedY *= -1;
+        }
+    }
+
     canvas.addEventListener("click", shoot);
 
     const animateDuck = () => {
         setTimeout(() => {
+            updateDuckPosition();
             drawDuck();
             requestAnimationFrame(animateDuck);
         }, 100);
     }
+
+    const animateFallingDuck = () => {
+        let fallingDuckX = duckX;
+        let fallingDuckY = duckY;
+
+        const fallingDuckAnimation = () => {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            fallingDuckY += 5;
+
+            ctx.drawImage(fallingDuckSprites[currentFrameIndex], fallingDuckX, fallingDuckY, duckWidth, duckHeight);
+        
+            if (fallingDuckY < canvas.height) {
+                requestAnimationFrame(fallingDuckAnimation)
+            }
+        };
+
+        fallingDuckAnimation();
+    }
+
+    setInterval(() => {
+        duckSpeedX = Math.random() * 10 -5;
+        duckSpeedY = Math.random() * 10 -5;
+    }, 5000);
 
     animateDuck();
 });
