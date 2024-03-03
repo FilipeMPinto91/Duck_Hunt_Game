d0850c0eb8ab263975a6f9dfa4b3d98b028f19dc
JoaoRibeiroMoutinho <joaocmribeiro1@gmail.com>
2024-03-03 21:51:15 +0000

    clean up

--- scripts/dog.js
+++ scripts/dog.js
@@ -1,65 +1,65 @@
 function launchWalkoutAnimation() {
-  const dogElement = document.createElement("div");
-  const gameContainer = document.getElementById("game-container");
-  dogElement.setAttribute("class", "dog");
-  gameContainer.appendChild(dogElement);
-  dogElement.addEventListener("animationend", () => {
-    dogElement.classList.add("found");
-  });
-  dogElement.addEventListener("animationend", () => {
-    setTimeout(() => {
-      dogElement.classList.add("jump");
-    }, 1000);
-  });
-  dogElement.addEventListener("animationend", () => {
-    setTimeout(() => {
-      dogElement.remove();
-    }, 1800);
-  });
+    const dogElement = document.createElement("div");
+    const gameContainer = document.getElementById("game-container");
+    dogElement.setAttribute("class", "dog");
+    gameContainer.appendChild(dogElement);
+    dogElement.addEventListener("animationend", () => {
+        dogElement.classList.add("found");
+    });
+    dogElement.addEventListener("animationend", () => {
+        setTimeout(() => {
+            dogElement.classList.add("jump");
+        }, 1000);
+    });
+    dogElement.addEventListener("animationend", () => {
+        setTimeout(() => {
+            dogElement.remove();
+        }, 1800);
+    });
 }
+
 function startNewRound() {
-  ducksKilled = 0;
-  bulletCounter = 3;
+    ducksKilled = 0;
+    bulletCounter = 3;
 }
+
 function endGame() {
-  isGameOver = true;
-  const gameOverElement = document.getElementById("game-over");
-  gameOverElement.style.display = "block";
-  const scoreElement = document.getElementById("score");
-  scoreElement.textContent = totalDucksKilled * 100;
+    isGameOver = true;
+    const gameOverElement = document.getElementById("game-over");
+    gameOverElement.style.display = "block";
+    const scoreElement = document.getElementById("score");
+    scoreElement.textContent = totalDucksKilled * 100;
 
-  const canvas = document.getElementById('canvas');
-  const ctx = canvas.getContext('2d');
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  // Stop the ducks from spawning
-  clearTimeout(flyOutTimeOut);
+    clearTimeout(flyOutTimeOut);
 
-  // Wait for 2 seconds and then start the game again
-  setTimeout(() => {
-    isGameOver = false; // Reset the isGameOver variable
-    startGame();
-  }, 2000);
+    setTimeout(() => {
+        isGameOver = false;
+        startGame();
+    }, 2000);
 }
 
 function showDuck(numberOfDucksKilled) {
-  let dogElement = document.createElement("div");
-  let gameContainer = document.getElementById("game-container");
-  dogElement.setAttribute("class", "dog2");
-  gameContainer.appendChild(dogElement);
-  if (numberOfDucksKilled === 1) {
-    dogElement.classList.add("got-one");
-  } else if (numberOfDucksKilled === 2) {
-    dogElement.classList.add("got-two");
-  } else {
-    return;
-  }
+    let dogElement = document.createElement("div");
+    let gameContainer = document.getElementById("game-container");
+    dogElement.setAttribute("class", "dog2");
+    gameContainer.appendChild(dogElement);
+    if (numberOfDucksKilled === 1) {
+        dogElement.classList.add("got-one");
+    } else if (numberOfDucksKilled === 2) {
+        dogElement.classList.add("got-two");
+    } else {
+        dogElement.classList.add("got-one");
+    }
 }
 
 function dogLaugh() {
-  let dogElement = document.createElement("div");
-  let dogContainer = document.getElementById("game-container");
-  dogElement.setAttribute("class", "laugh");
-  dogContainer.appendChild(dogElement);
-  dogElement.classList.add("laugh");
+    let dogElement = document.createElement("div");
+    let dogContainer = document.getElementById("game-container");
+    dogElement.setAttribute("class", "laugh");
+    dogContainer.appendChild(dogElement);
+    dogElement.classList.add("laugh");
 }
